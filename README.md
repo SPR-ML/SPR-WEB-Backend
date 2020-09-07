6959fd32e3a303e1a594acca81327ee15f2ebc8c
Cyame <1362941473@qq.com>
2020-09-07 15:49:01 +0800

    feat(runme&readme): add runme as main-setup, add package for cz friendly. with comment in readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# StudentPerformanceRegression-WEB-Backend
-The backend of exhibiting page.
+# 学业表现回归模型-项目展示后端服务
+
+![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)
+![FastAPI](https://img.shields.io/badge/FastAPI-v0.61.1+-brightgreen.svg)
+
+使用FastAPI构建的展示页后端服务。
+
+## 使用说明
+
+
+## 注意事项
+### Commitzen友好
+
+请使用`git cz`进行Commit而非`git commit` 
---
+++ runme.py
@@ -0,0 +1,48 @@
+import json
+# from urllib.parse import parse_qs
+from fastapi import FastAPI
+# from wsgiref.simple_server import make_server
+from pydantic import BaseModel
+# Python WEB接口开发 Flask -> FastAPI
+
+
+app = FastAPI()
+
+@app.get('/test')
+def test():
+    return {'message': 'Init FastAPI successfully.'}
+
+@app.get('/{scale}/column/target')
+def getRawTarget(scale: str):
+    if scale == 'ext':
+        return {'message': '请求扩展数据集G3中'}
+    elif scale == 'raw':
+        return {'message': '请求原始G3中'}
+    else:
+        return {'message': '未找到请求目标'}
+
+@app.get('/{scale}/column/{colType}/{col}')
+def getRawColumn(scale: str,colType: str, col: str):
+    return {'message': '请求{}数据集中{}类型的{}数据统计'.format(scale,colType, col)}
+
+@app.get('/model/{modelType}/{target}')
+def getModel(modelType: str, target: str):
+    return {'message': '请求{}模型中的{}项'.format(modelType,target)}
+
+class form:
+    formData = {}
+    def __init__(self, data):
+        self.formData = data
+
+class dataset:
+    fileUrl = ''
+    def __init__(self, url):
+        self.fileUrl = url
+
+@app.post('/predict/form')
+def getPredict(form: str):
+    return {'message': '预测单个项目'}
+
+@app.post('/predict/dataset')
+def getPredictSet(dataset: str):
+    return {'message': '预测数据集'}
