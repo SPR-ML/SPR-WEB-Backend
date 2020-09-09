10e8ca7c605b5fed0d14f9146f1d287bac024020
Cyame <1362941473@qq.com>
2020-09-10 02:21:26 +0800

    feat(model supportion): support model

--- modules/models.py
+++ modules/models.py
@@ -3,10 +3,19 @@ import joblib
 import pandas as pd
 import modules.utils as ut
 from sklearn.preprocessing import LabelEncoder
+import logging
 
-availableModels = [
+# availableModels = [
+#     'v1_GaussianNB_Classifier_4.m', 'v1_MultiNB_Classifier_4.m',
+#     'v2_GaussianNB_Classifier_2.m', 'v2_MultiNB_Classifier_2.m',
+#     'v3_XGBRegressor.m', 'v4_CatBoostRegressor.m'
+# ]
+availableNoModels = [
     'v1_GaussianNB_Classifier_4.m', 'v1_MultiNB_Classifier_4.m',
-    'v2_GaussianNB_Classifier_2.m', 'v2_MultiNB_Classifier_2.m',
+    'v2_GaussianNB_Classifier_2.m', 'v2_MultiNB_Classifier_2.m'
+]
+
+availablePreModels = [
     'v3_XGBRegressor.m', 'v4_CatBoostRegressor.m'
 ]
 
@@ -44,6 +53,7 @@ def importModel(model):
 def runWithNoProcess(model, data: pd.DataFrame):
     clf = importModel(model)
     data = dataTransform(data)
+    print("[NAN]TO MODEL", data)
     res = clf.predict(data)
     return res
     pass
@@ -53,6 +63,7 @@ def runWithPreProcess(model, data: pd.DataFrame):
     clf = importModel(model)
     data = dataTransform(data)
     data = dataPreProcessing(data)
+    print("[PRE]TO MODEL",data)
     res = clf.predict(data)
     return res
 
@@ -60,13 +71,51 @@ def runWithPreProcess(model, data: pd.DataFrame):
 # In stucture order
 def predictForm(form, process=True):
     predictResult = []
+    thisForm = vars(form)
+    dataSet = {}
+    print("this: ",thisForm)
+    # logging.info("")
+    dataSet['school'] = thisForm['School']
+    dataSet['class'] = thisForm['Class']
+    dataSet['age'] = thisForm['Age']
+    dataSet['address'] = thisForm['Address']
+    dataSet['famsize'] = thisForm['Famsize']
+    dataSet['Pstatus'] = thisForm['Pstatus']
+    dataSet['Medu'] = thisForm['Medu']
+    dataSet['Fedu'] = thisForm['Fedu']
+    dataSet['Mjob'] = thisForm['Mjob']
+    dataSet['Fjob'] = thisForm['Fjob']
+    dataSet['reason'] = thisForm['Reason']
+    dataSet['guardian'] = thisForm['Guardian']
+    dataSet['traveltime'] = thisForm['TravelTime']
+    dataSet['studytime'] = thisForm['StudyTime']
+    dataSet['failures'] = thisForm['Failures']
+    dataSet['schoolsup'] = thisForm['SchoolSup']
+    dataSet['famsup'] = thisForm['FamSup']
+    dataSet['paid'] = thisForm['Paid']
+    dataSet['activities'] = thisForm['Activities']
+    dataSet['nursery'] = thisForm['Nursery']
+    dataSet['higher'] = thisForm['Higher']
+    dataSet['internet'] = thisForm['Internet']
+    dataSet['romantic'] = thisForm['Romantic']
+    dataSet['famrel'] = thisForm['FamRel']
+    dataSet['freetime'] = thisForm['FreeTime']
+    dataSet['goout'] = thisForm['GoOut']
+    dataSet['Dalc'] = thisForm['Dalc']
+    dataSet['Walc'] = thisForm['Walc']
+    dataSet['health'] = thisForm['Health']
+    dataSet['absences'] = thisForm['Absences']
+
+    # print("multi:", dataSet)
+
     for m in availableModels:
+
         if process:
             predictResult.append(
-                runWithPreProcess(pd.DataFrame.from_dict(vars(form))))
+                runWithPreProcess(m, pd.DataFrame.from_dict([dataSet])))
         else:
             predictResult.append(
-                runWithNoProcess(pd.DataFrame.from_dict(vars(form))))
+                runWithNoProcess(m, pd.DataFrame.from_dict([dataSet])))
     return predictResult
     # form = [value for value in args]
 
